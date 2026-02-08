d408a0f154ae340096caed7cd1abfa726c52e275
Sadik-Sami <miasimanto@gmail.com>
2026-02-08 17:06:05 +0600

    base commit

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,14 +1,6 @@
-import { env } from "@multiCommerece/env/server";
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import ws from "ws";
-
-import * as schema from "./schema";
-
-neonConfig.webSocketConstructor = ws;
-
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true
+import { env } from '@multiCommerece/env/server';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 const sql = neon(env.DATABASE_URL);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(sql);
